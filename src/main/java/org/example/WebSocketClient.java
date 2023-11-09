687550af41a1dcc1f16d462c3f31dc8c08a352c8
Aleksandr Abramov <AleksAbramovWEB@yandex.ru>
2023-11-09 11:43:47 +0300

    1

---
+++ src/main/java/org/example/WebSocketClient.java
@@ -0,0 +1,56 @@
+package org.example;
+
+import okhttp3.*;
+
+import java.util.concurrent.TimeUnit;
+
+public class WebSocketClient {
+
+    private static final int MAX_CONNECTIONS = 1000;
+    private static final String URL = "";
+
+    public static int total = 0;
+
+    public static void main(String[] args) throws InterruptedException {
+
+        for (int i = 1; i < MAX_CONNECTIONS; i++) {
+            OkHttpClient client = new OkHttpClient.Builder()
+                    .build();
+
+            Request request = new Request.Builder()
+                    .url(URL)
+                    .build();
+
+            client.newWebSocket(request, new WebSocketListener() {
+                final int finalI = total;
+
+                @Override
+                public void onOpen(WebSocket webSocket, Response response) {
+                    total++;
+                    System.out.println("WebSocket connection opened " + finalI + " total: " + total);
+                }
+
+                @Override
+                public void onMessage(WebSocket webSocket, String text) {
+                    System.out.println("Received message " + finalI + ": " + text);
+                }
+
+                @Override
+                public void onFailure(WebSocket webSocket, Throwable t, Response response) {
+                    t.printStackTrace();
+                }
+
+                @Override
+                public void onClosed(WebSocket webSocket, int code, String reason) {
+                    System.out.println("WebSocket onClosed " + finalI + ": " + reason);
+                }
+
+                @Override
+                public void onClosing(WebSocket webSocket, int code, String reason) {
+                    System.out.println("WebSocket onClosing " + finalI + ": " + reason);
+                }
+            });
+            TimeUnit.SECONDS.sleep(1);
+        }
+    }
+}
